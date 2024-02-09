844d89a0dfda38f6532947a34925557d949801d9
Aamil Vahora <aamilvahora786@gmail.com>
2024-02-09 02:23:31 -0500

    fix

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -26,7 +26,7 @@ export default function AboutMe() {
       </div>
       <div className='intro_container'>
         <p className='intro'>
-          My Name is Aamil vahora and I grew up in the Catonsville, Maryland Area My whole life. I attended Mount Hebron High School, and I Currently go to the Univeristy of Maryland baltimore County. Some things i like to do on my free time is play sports, watch movies and cook 
+          My Name is Aamil Vahora and I grew up in the Catonsville, Maryland Area My whole life. I attended Mount Hebron High School, and I Currently go to the University of Maryland Baltimore County. Some things I like to do on my free time is play sports, watch movies, and cook.
         </p>
       </div>
       <div className="links-container">
@@ -41,123 +41,112 @@ export default function AboutMe() {
       </div>
 
       <div className='cards-container'>
-  <div className='card-space'>
-    <Card 
-      imagePath="/images/Matlab_logo.jpeg" 
-      description="Matlab"
-    />
-  </div>
-  <div className='card-space'>
-    <Card 
-      imagePath="/images/python.png" 
-      description="Python"
-    />
-  </div>
-  <div className='card-space'>
-    <Card 
-      imagePath="/images/Ros.jpeg" 
-      description="Ros Robotics System"
-    />
-  </div>
-  <div className='card-space'>
-    <Card 
-      imagePath="/images/Gitlab.png" 
-      description="Gitlab"
-    />
-  </div>
-  <div className='card-space'>
-    <Card 
-      imagePath="/images/Gihtub.jpeg" 
-      description="Github"
-    />
-  </div>
-  <div className='card-space'>
-    <Card 
-      imagePath="/images/conda2.png" 
-      description="Conda"
-    />
-  </div>
-
-  <div className='card-space'>
-    <Card 
-      imagePath="/images/86.png" 
-      description="Assembly"
-    />
-  </div>
-  <div className='card-space'>
-    <Card 
-      imagePath="/images/C.png" 
-      description="C"
-    />
-  </div>
-  <div className='card-space'>
-    <Card 
-      imagePath="/images/C++.jpeg" 
-      description="C++"
-    />
-  </div>
-
-
-  </div>
-
-
-
-
+        <div className='card-space'>
+          <Card 
+            imagePath={MatlabLogo}
+            description="Matlab"
+          />
+        </div>
+        <div className='card-space'>
+          <Card 
+            imagePath={PythonLogo}
+            description="Python"
+          />
+        </div>
+        <div className='card-space'>
+          <Card 
+            imagePath={RosLogo}
+            description="Ros Robotics System"
+          />
+        </div>
+        <div className='card-space'>
+          <Card 
+            imagePath={GitlabLogo}
+            description="Gitlab"
+          />
+        </div>
+        <div className='card-space'>
+          <Card 
+            imagePath={GithubLogo}
+            description="Github"
+          />
+        </div>
+        <div className='card-space'>
+          <Card 
+            imagePath={CondaLogo}
+            description="Conda"
+          />
+        </div>
+        <div className='card-space'>
+          <Card 
+            imagePath={AssemblyLogo}
+            description="Assembly"
+          />
+        </div>
+        <div className='card-space'>
+          <Card 
+            imagePath={CLogo}
+            description="C"
+          />
+        </div>
+        <div className='card-space'>
+          <Card 
+            imagePath={CppLogo}
+            description="C++"
+          />
+        </div>
+      </div>
 
       <div className='Sports'>
         <h2 className='Sports-Title'>
           Sports
         </h2>
       </div>
-<div className='cards-container'>
-  <div className='card-space'>
-    <Card 
-      imagePath="/images/bjj.jpeg" 
-      description="On my Free time I love Practicing Brazilian Jiu-Jitsu, I go to a gym called Crazy 88 MMA and practice regularly"
-    />
-  </div>
-  <div className='card-space'>
-    <Card 
-      imagePath="/images/spikeball.jpeg" 
-      description="I also Love Playing SpikeBall especially on the beach, i play often with my friends whenever i get the chance"
-    />
-  </div>
-  <div className='card-space'>
-    <Card 
-      imagePath="/images/football.jpeg" 
-      description="I love watching Football and sometimes playing the sport, my favorite team is the Baltimore Ravens, they almost made it all the way this year :("
-    />
-  </div>
-  </div>
-  <div className='Sports'>
+      <div className='cards-container'>
+        <div className='card-space'>
+          <Card 
+            imagePath={BjjLogo}
+            description="On my Free time I love Practicing Brazilian Jiu-Jitsu, I go to a gym called Crazy 88 MMA and practice regularly"
+          />
+        </div>
+        <div className='card-space'>
+          <Card 
+            imagePath={SpikeballLogo}
+            description="I also Love Playing SpikeBall especially on the beach, i play often with my friends whenever i get the chance"
+          />
+        </div>
+        <div className='card-space'>
+          <Card 
+            imagePath={FootballLogo}
+            description="I love watching Football and sometimes playing the sport, my favorite team is the Baltimore Ravens, they almost made it all the way this year :("
+          />
+        </div>
+      </div>
+      <div className='Sports'>
         <h2 className='Sports-Title'>
           Cooking/Eating
         </h2>
+      </div>
+      <div className='cards-container'>
+        <div className='card-space'>
+          <Card 
+            imagePath={PastaLogo}
+            description="I love all types of Pasta. My favorite is Alfredo despite the image above lol."
+          />
         </div>
-        <div className='cards-container'>
-  <div className='card-space'>
-    <Card 
-      imagePath="/images/pasta.jpeg" 
-      description="I love all types of Pasta My favorite is alfredo despite the image above lol"
-    />
-  </div>
-  <div className='card-space'> 
-    <Card 
-      imagePath="/images/sushi.jpeg" 
-      description="I could eat Sushi all day, I wasnt the biggest fan when i was younger, but i can tear up a shrimp tempura roll easily now"
-    />
-    </div>
-      <div className='card-space'>
-        <Card 
-          imagePath="/images/butterchicken.jpeg" 
-          description="I am from India so my mom has taught me how to cook all types of food including butter chicken which is my favorite"
-        />
+        <div className='card-space'> 
+          <Card 
+            imagePath={SushiLogo}
+            description="I could eat Sushi all day, I wasn't the biggest fan when I was younger, but I can tear up a shrimp tempura roll easily now."
+          />
+        </div>
+        <div className='card-space'>
+          <Card 
+            imagePath={ButterChickenLogo}
+            description="I am from India so my mom has taught me how to cook all types of food including butter chicken which is my favorite."
+          />
+        </div>
       </div>
     </div>
-
-    </div>
-
-    
-    
   );
 }
