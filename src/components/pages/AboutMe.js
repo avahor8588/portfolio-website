a32c591b04b9c50d98343ed5b38ece4e42d77223
Aamil Vahora <aamilvahora786@gmail.com>
2024-02-09 01:01:39 -0500

    hopefully fixed

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -33,74 +33,56 @@ export default function AboutMe() {
         <a href="https://docs.google.com/document/d/1t_1MGW4__XyzQae20U9LPr1nw_2f3iqA/edit?usp=sharing&ouid=117331970786774132442&rtpof=true&sd=true" className="link-button" target="_blank" rel="noopener noreferrer">My Resume</a>
         <a href="https://docs.google.com/document/d/1GWeEb8rTKP2QwmrxAeC4tvYDZzey2RrX/edit?usp=sharing&ouid=117331970786774132442&rtpof=true&sd=true" className="link-button" target="_blank" rel="noopener noreferrer">My Transcript</a>
       </div>
-    <div className='my-info'>
-      {/* Skills Section */}
       <div className='Sports'>
-        <h2 className='Sports-Title'>Skills</h2>
+        <h2 className='Sports-Title'>
+          Sports
+        </h2>
       </div>
-      <div className='cards-container'>
-        <div className='card-space'>
-          <Card imagePath={MatlabLogo} description="Matlab" />
+<div className='cards-container'>
+  <div className='card-space'>
+    <Card 
+      imagePath="/images/bjj.jpeg" 
+      description="On my Free time I love Practicing Brazilian Jiu-Jitsu, I go to a gym called Crazy 88 MMA and practice regularly"
+    />
+  </div>
+  <div className='card-space'>
+    <Card 
+      imagePath="/images/spikeball.jpeg" 
+      description="I also Love Playing SpikeBall especially on the beach, i play often with my friends whenever i get the chance"
+    />
+  </div>
+  <div className='card-space'>
+    <Card 
+      imagePath="/images/football.jpeg" 
+      description="I love watching Football and sometimes playing the sport, my favorite team is the Baltimore Ravens, they almost made it all the way this year :("
+    />
+  </div>
+  </div>
+  <div className='Sports'>
+        <h2 className='Sports-Title'>
+          Cooking/Eating
+        </h2>
         </div>
-        <div className='card-space'>
-          <Card imagePath={PythonLogo} description="Python" />
-        </div>
-        <div className='card-space'>
-          <Card imagePath={RosLogo} description="Ros Robotics System" />
-        </div>
-        <div className='card-space'>
-          <Card imagePath={GitlabLogo} description="Gitlab" />
-        </div>
-        <div className='card-space'>
-          <Card imagePath={GithubLogo} description="Github" />
-        </div>
-        <div className='card-space'>
-          <Card imagePath={CondaLogo} description="Conda" />
-        </div>
-        <div className='card-space'>
-          <Card imagePath={AssemblyLogo} description="Assembly" />
-        </div>
-        <div className='card-space'>
-          <Card imagePath={CLogo} description="C" />
-        </div>
-        <div className='card-space'>
-          <Card imagePath={CppLogo} description="C++" />
-        </div>
-      </div>
-
-      {/* Sports Section */}
-      <div className='Sports'>
-        <h2 className='Sports-Title'>Sports</h2>
-      </div>
-      <div className='cards-container'>
-        <div className='card-space'>
-          <Card imagePath={BjjLogo} description="On my Free time I love Practicing Brazilian Jiu-Jitsu, I go to a gym called Crazy 88 MMA and practice regularly" />
-        </div>
-        <div className='card-space'>
-          <Card imagePath={SpikeballLogo} description="I also Love Playing SpikeBall especially on the beach, i play often with my friends whenever i get the chance" />
-        </div>
-        <div className='card-space'>
-          <Card imagePath={FootballLogo} description="I love watching Football and sometimes playing the sport, my favorite team is the Baltimore Ravens, they almost made it all the way this year :(" />
-        </div>
-      </div>
-
-      {/* Cooking/Eating Section */}
-      <div className='Sports'>
-        <h2 className='Sports-Title'>Cooking/Eating</h2>
-      </div>
-      <div className='cards-container'>
-        <div className='card-space'>
-          <Card imagePath={PastaLogo} description="I love all types of Pasta My favorite is alfredo despite the image above lol" />
-        </div>
-        <div className='card-space'>
-          <Card imagePath={SushiLogo} description="I could eat Sushi all day, I wasn't the biggest fan when i was younger, but i can tear up a shrimp tempura roll easily now" />
-        </div>
-        <div className='card-space'>
-          <Card imagePath={ButterChickenLogo} description="I am from India so my mom has taught me how to cook all types of food including butter chicken which is my favorite" />
-        </div>
+        <div className='cards-container'>
+  <div className='card-space'>
+    <Card 
+      imagePath="/images/pasta.jpeg" 
+      description="I love all types of Pasta My favorite is alfredo despite the image above lol"
+    />
+  </div>
+  <div className='card-space'> 
+    <Card 
+      imagePath="/images/sushi.jpeg" 
+      description="I could eat Sushi all day, I wasnt the biggest fan when i was younger, but i can tear up a shrimp tempura roll easily now"
+    />
+    </div>
+      <div className='card-space'>
+        <Card 
+          imagePath="/images/butterchicken.jpeg" 
+          description="I am from India so my mom has taught me how to cook all types of food including butter chicken which is my favorite"
+        />
       </div>
     </div>
-      
 
     </div>
 
--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -2,11 +2,11 @@
 import React from 'react';
 import './HomePage.css'; // Make sure to create this CSS file
 import SocialLinks from '../SocialLinks';
-import ME from './Images/boatME.JPG';
+
 function HomePage() {
   return (
     <div className="home-container">
-      <img src={ME} alt="Me" className="my-photo"/>
+      <img src="/images/boatMe.jpg" alt="Me" className="my-photo"/>
       <p className="blurb">Welcome to my website!, Here's a little about me...</p>
       <SocialLinks />
     </div>
