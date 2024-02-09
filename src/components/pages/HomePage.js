eeb4c5e2aa54d895dba70fa440d7f5657116d93f
Aamil Vahora <aamilvahora786@gmail.com>
2024-02-09 02:40:57 -0500

    done

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -2,11 +2,12 @@
 import React from 'react';
 import './HomePage.css'; // Make sure to create this CSS file
 import SocialLinks from '../SocialLinks';
+import boat from "./boatME.JPG";
 
 function HomePage() {
   return (
     <div className="home-container">
-      <img src="/images/boatMe.jpg" alt="Me" className="my-photo"/>
+      <img src={boat} alt="Me" className="my-photo"/>
       <p className="blurb">Welcome to my website!, Here's a little about me...</p>
       <SocialLinks />
     </div>
